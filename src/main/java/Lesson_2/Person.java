2e09feee9adaf799a16a8edb2d95adac11b43443
DevDan93 <andreevdangit@gmail.com>
2025-11-18 22:43:27 +0500

    les2.2

---
+++ src/main/java/Lesson_2/Person.java
@@ -0,0 +1,79 @@
+package Lesson_2;
+
+public class Person {
+    private String gender;
+    private String name;
+    private int height;
+    private int weight;
+    private int age;
+
+
+    public Person(String gender, String name, int height, int weight, int age) {
+        this.gender = gender;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public void walk() {
+        System.out.println(name + "идет");
+    }
+
+    public void sit() {
+        System.out.println(name + "сидит");
+    }
+
+    public void run() {
+        System.out.println(name + "бежит");
+    }
+
+    public void talk() {
+        System.out.println(name + "говорит");
+    }
+
+    public void learnJava() {
+        System.out.println(name + "учит джаву");
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
